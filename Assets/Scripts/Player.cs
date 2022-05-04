04ceb2b135eb5ff77ef8e82b4d2f577516e74e7c
MauroDot <tmaurodot@gmail.com>
2022-05-03 23:19:00 -0500

    2D Space Game Practice 1st commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tak the current position before the first frame update
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        calculateMovement();
+    }
+
+    void calculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x > 11.27f)
+        {
+            transform.position = new Vector3(-11.27f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.27f)
+        {
+            transform.position = new Vector3(11.27f, transform.position.y, 0);
+        }
+    }
+}
